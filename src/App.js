5a2befec796ee3e9fed282aee687e32af6adda65
AhmedAbdAlZaher <ahmed.abdalzaher@outlook.com>
2023-09-09 20:17:06 +0300

    comit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Home from './Components/MyHome/Home';
 
 import Navbar from './Components/Navbar/Navbar'
 
 function App() {
   return (<>
   <Navbar></Navbar>
+  <Home></Home>
   </>
     
   );
---
+++ src/Components/MyHome/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default function Home() {
+
+  return (
+   <div className='d-flex justify-content-center align-items-center'>
+   
+    <h2> hello from home</h2>
+   </div>
+  )
+}
