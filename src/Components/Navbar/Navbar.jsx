335af5b1303bf79f67f9fae0ed0f904af5fd5fa8
AhmedAbdAlZaher <ahmed.abdalzaher@outlook.com>
2023-09-09 23:28:08 +0300

    udno this

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,25 +7,15 @@ import './Navbar.css'
 
 export default function navbar() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary myNavbar ">
+    <Navbar expand="lg" className="bg-body-tertiary myNavbar py-4 ">
     <Container>
       <Navbar.Brand className='text-white' href="#home">React-Bootstrap</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
+        <Nav className="ms-auto">
+          <Nav.Link className='text-white' href="#home">Home</Nav.Link>
+          <Nav.Link className='text-white' href="#link">Portfolio</Nav.Link>
+          <Nav.Link className='text-white' href="#link">Contact</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Container>
