27aa8495ec9b177b66484ca002aa67e9453e8db5
AhmedAbdAlZaher <ahmed.abdalzaher@outlook.com>
2023-09-15 09:46:11 +0300

    port

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
-
+import img1 from '../../Imgs/poert1.png'
+import img2 from '../../Imgs/port2.png'
+import img3 from '../../Imgs/port3.png'
 export default function Portfolio() {
   return (
-    <div>Portfolio</div>
+    <>
+    <section className='Home d-flex flex-column justify-content-center align-items-center'>
+      <div className='contianer'>
+        <div className='row'>
+          <div className='Main col-md-6 position-relative'>
+            <img src={img1} alt="" />
+            <div className='overlayer position-absolute start-0 end-0 w-100 top-0 h-100 d-flex justify-content-center align-items-center'>
+            <i _ngcontent-osb-c19="" className="text-white fa-solid fa-plus fa-6x"></i>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      </section>
+    </>
   )
 }
